2d84fa1b90c12543d2e48f49de0a153be0e532c2
juan jose barrera <juanjoseb9805@gmail.com>
2025-06-21 10:45:18 -0500

    solution coding challenge 3 about data structure

--- app.js
+++ app.js
@@ -1,57 +1,106 @@
-const game = {
-  team1: "team Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "team Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // Solution 1
+// for (const [i, name] of game.scored.entries()) {
+//   console.log(`${name} ${i + 1}`);
+// }
+
+// // Solution 2
+// let avg = 0;
+// for (const odd of Object.values(game.odds)) {
+//   console.log(odd);
+//   avg += odd;
+// }
+// console.log(avg / Object.entries(game.odds).length);
+
+// // Solution 3
+// for (const [team, odd] of Object.entries(game.odds))
+//   console.log(`odd of victory ${team}: ${odd}`);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
 
 // Solution 1
-for (const [i, name] of game.scored.entries()) {
-  console.log(`${name} ${i + 1}`);
-}
+// console.log(gameEvents.values());
+
+const arr = [...gameEvents.values()];
+
+console.log(arr);
+
+// console.log(gameEvents);
+
+const eventsSet = new Set(arr);
+console.log(eventsSet);
 
 // Solution 2
-let avg = 0;
-for (const odd of Object.values(game.odds)) {
-  console.log(odd);
-  avg += odd;
-}
-console.log(avg / Object.entries(game.odds).length);
+gameEvents.delete(64);
+console.log(gameEvents);
 
 // Solution 3
-for (const [team, odd] of Object.entries(game.odds))
-  console.log(`odd of victory ${team}: ${odd}`);
+for (const [minute, event] of gameEvents) {
+  console.log(`${event} (${minute < 45 ? "first time" : "second time"})`);
+}
