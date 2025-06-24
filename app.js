62ffc00f2a41b1c363177e054f6564a299e7426f
juan jose barrera <juanjoseb9805@gmail.com>
2025-06-24 10:24:31 -0500

    challenge number 4 strings: they are two solutions the first one was done by me but, just complete like the 60% off the challenge, the other one was done by me again but then to watch the solution of it

--- app.js
+++ app.js
@@ -84,23 +84,102 @@ const gameEvents = new Map([
   [92, "🔶 Yellow card"],
 ]);
 
-// Solution 1
-// console.log(gameEvents.values());
+// // Solution 1
+// // console.log(gameEvents.values());
 
-const arr = [...gameEvents.values()];
+// const arr = [...gameEvents.values()];
 
-console.log(arr);
+// console.log(arr);
 
+// // console.log(gameEvents);
+
+// const eventsSet = new Set(arr);
+// console.log(eventsSet);
+
+// // Solution 2
+// gameEvents.delete(64);
 // console.log(gameEvents);
 
-const eventsSet = new Set(arr);
-console.log(eventsSet);
+// // Solution 3
+// for (const [minute, event] of gameEvents) {
+//   console.log(`${event} (${minute < 45 ? "first time" : "second time"})`);
+// }
+//
 
-// Solution 2
-gameEvents.delete(64);
-console.log(gameEvents);
+// Coding challenge #4 Strings
 
-// Solution 3
-for (const [minute, event] of gameEvents) {
-  console.log(`${event} (${minute < 45 ? "first time" : "second time"})`);
-}
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+// THIS TEST DATA (pasted to textarea)
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+// Afterwards, test with your own test data!
+
+// GOOD LUCK 😀
+
+// const textarea = document.body.append(document.createElement("textarea"));
+// const button = document.body.append(document.createElement("button"));
+
+// const text = document.querySelector("textarea").value;
+
+// document.querySelector("button").addEventListener("click", function () {
+//   const input = text.toLowerCase();
+
+//   textarea.value = input;
+// });
+
+// console.log(document.querySelector("textarea").value);
+
+// console.log("     HI juan S da ".trim().toLowerCase());
+
+const convert = function () {
+  const input = prompt("send a msg");
+  const textL = input.toLowerCase();
+  const messages = textL.split("_");
+
+  const messagesUp = [];
+
+  for (const message of messages) {
+    messagesUp.push(message.replace(message[0], message[0].toUpperCase()));
+  }
+  console.log(messagesUp.join("").trim());
+};
+
+const convert1 = function () {
+  const message = prompt();
+  // console.log(message);
+
+  const rows = message.split("\n");
+  // console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(output);
+  }
+};
+
+convert();
+convert1();
