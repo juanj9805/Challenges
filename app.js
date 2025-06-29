b6314d19e4024c32ae9428e205b48db936d98af7
juan jose barrera <juanjoseb9805@gmail.com>
2025-06-28 20:03:39 -0500

    Here is the first challenge about advanced functions, I complete 2 of the 4 requirements without to be able to apply partial application

--- app.js
+++ app.js
@@ -151,69 +151,69 @@ const gameEvents = new Map([
 // console.log("     HI juan S da ".trim().toLowerCase());
 
 //
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// 🔴 Delayed Departure from FAO to TXL (11h25)
-//              Arrival from BRU to FAO (11h45)
-//   🔴 Delayed Arrival from HEL to FAO (12h05)
-//            Departure from FAO to LIS (12h30)
+// // 🔴 Delayed Departure from FAO to TXL (11h25)
+// //              Arrival from BRU to FAO (11h45)
+// //   🔴 Delayed Arrival from HEL to FAO (12h05)
+// //            Departure from FAO to LIS (12h30)
 
-const op = function (msg) {
-  const sentences = msg.split("+");
-  // console.log(sentences);
+// const op = function (msg) {
+//   const sentences = msg.split("+");
+//   // console.log(sentences);
 
-  const arr = [];
+//   const arr = [];
 
-  for (const sentence of sentences) {
-    // console.log(sentence);
-    const [info, from, to, time] = sentence.split(";");
-    const iok = info.slice(1).replace("_", " ");
-    const fok = from.slice(0, 3).toUpperCase();
-    const tok = to.slice(0, 3).toUpperCase();
+//   for (const sentence of sentences) {
+//     // console.log(sentence);
+//     const [info, from, to, time] = sentence.split(";");
+//     const iok = info.slice(1).replace("_", " ");
+//     const fok = from.slice(0, 3).toUpperCase();
+//     const tok = to.slice(0, 3).toUpperCase();
 
-    console.log(
-      `${
-        iok.includes("Delayed") ? "😒 " : ""
-      }${iok} from ${fok} to ${tok} (${time})`
-    );
-  }
-};
+//     console.log(
+//       `${
+//         iok.includes("Delayed") ? "😒 " : ""
+//       }${iok} from ${fok} to ${tok} (${time})`
+//     );
+//   }
+// };
 
-op(flights);
+// op(flights);
 
-const ok = function (msg) {
-  const [uno, dos, tres, cuatro] = msg.split("+");
+// const ok = function (msg) {
+//   const [uno, dos, tres, cuatro] = msg.split("+");
 
-  const [info, from, to, time] = uno.split(";");
-  const iok = info.slice(1).replace("_", " ");
-  const fok = from.slice(0, 3).toUpperCase();
-  const tok = to.slice(0, 3).toUpperCase();
+//   const [info, from, to, time] = uno.split(";");
+//   const iok = info.slice(1).replace("_", " ");
+//   const fok = from.slice(0, 3).toUpperCase();
+//   const tok = to.slice(0, 3).toUpperCase();
 
-  // console.log(time);
-  // console.log(from);
-  // console.log(to);
-  // console.log(time);
-  // const tiok =
+// console.log(time);
+// console.log(from);
+// console.log(to);
+// console.log(time);
+// const tiok =
 
-  // console.log(
-  //   `${
-  //     iok.includes("Departures") ? iok.padStart(30, "ok") : ""
-  //   } ${iok} from ${fok} to ${tok} (${time})`
-  // );
+// console.log(
+//   `${
+//     iok.includes("Departures") ? iok.padStart(30, "ok") : ""
+//   } ${iok} from ${fok} to ${tok} (${time})`
+// );
 
-  console.log(
-    `${
-      iok.includes("Delayed") ? "😒 " : ""
-    }${iok} from ${fok} to ${tok} (${time})`
-  );
+// console.log(
+//   `${
+//     iok.includes("Delayed") ? "😒 " : ""
+//   }${iok} from ${fok} to ${tok} (${time})`
+// );
 
-  // uno.replaceAll("_", " ");
-  // .replaceAll(";", " ");
-  // console.log(un.padStart(30, "#"));
+// uno.replaceAll("_", " ");
+// .replaceAll(";", " ");
+// console.log(un.padStart(30, "#"));
 
-  const words = [];
-};
+// const words = [];
+// };
 //   for (const info of sentences) {
 //     const [status, country, destination, time] = info.split(";");
 //     // console.log(typeof status);
@@ -232,3 +232,63 @@ const ok = function (msg) {
 // console.log("juan");
 
 // ok(flights);
+
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)--------------------------------------------------------------------
+  
+  1.2. Based on the input number, update the a nswers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?) ---------------------------------------------------------------------------------
+2. Call this method whenever the user clicks the "Answer poll" button. ---------------------------
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const choice = Number(
+      prompt(
+        "What is your favourite programming language   0: JavaScript        1: Python        2: Rust        3: C++        (Write option number)"
+      )
+    );
+
+    if (choice <= 3 && choice === 3) {
+      // this.answers.push(choice);
+      // console.log(this.answers);
+      this.answers[3]++;
+      console.log(this.answers);
+    }
+  },
+};
+
+console.log(poll.answers);
+
+document
+  .querySelector(".btn")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
