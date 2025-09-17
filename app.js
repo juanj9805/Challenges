9dc0cd618404b67dec01cc53303c9d7f48369599
juan jose barrera <juanjoseb9805@gmail.com>
2025-09-17 07:32:08 -0500

    Second async challenge donde 3/6 points done by myself

--- app.js
+++ app.js
@@ -765,3 +765,62 @@ console.log(ford);
 // btn.addEventListener('click', function () {
 //   where(52.508, 13.381);
 // });
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+const bt = document.querySelector(".bt");
+const picture = document.querySelector(".picture");
+const newImg = document.createElement("img");
+
+const createImg = function (number) {
+  const path = `img/Screenshot_${number}.png`;
+  return setTimeout(() => {
+    new Promise(function (resolve, reject) {
+      newImg.src = path;
+      newImg.style.borderColor = "blue";
+      newImg.addEventListener("load", function () {
+        picture.append(newImg);
+        resolve(newImg);
+      });
+      img.addEventListener("error", function () {
+        reject(new Error("Img not found"));
+      });
+    }).then(
+      setTimeout(() => {
+        newImg.style.display = "none";
+        console.log("hi");
+      }, 2000)
+    );
+  }, 2000);
+};
+
+// bt.addEventListener("click", createImg);
+
+createImg(1)
+  .then((img) => {
+    console.log("good");
+  })
+  .catch((err) => console.error(err));
